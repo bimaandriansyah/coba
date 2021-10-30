64c2af34f85c42994533d85be39cb86fb438fc33
pras085 <ti.vindyprasetyo@gmail.com>
2021-10-30 17:04:54 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:coba/screens/home_screen.dart';
+import 'package:coba/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.orange,
       ),
-      home: const SplashScreen(),
+      home: const LoginScreen(),
     );
   }
 }
